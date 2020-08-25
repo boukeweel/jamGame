701628e41120c99c5bc267c3a38ed4355823aecb
Bouke <BoukeWeel@outlook.com>
2020-08-25 15:14:01 +0200

    movement and pressur pad

---
+++ jam game/Assets/scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D Rig;
+
+    public int Speed;
+    public int S_speed;
+
+    private bool Sprinting = false;
+
+
+    private void Start()
+    {
+        Rig = GetComponent<Rigidbody2D>();
+        S_speed = Speed;
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Speed -= S_speed;
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Speed = 8;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Rig.MovePosition(transform.position + M_input() * Time.deltaTime * Speed);
+
+    }
+    
+
+    public Vector3 M_input()
+    {
+        return new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
+    }
+}
---
+++ jam game/Assets/scripts/pressurpat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurpat : MonoBehaviour
+{
+
+    private bool SomeThingOn = false;
+
+    private SpriteRenderer Spr;
+
+    public Color color_;
+
+    private void Start()
+    {
+        Spr = GetComponent<SpriteRenderer>();  
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log(SomeThingOn);
+        }
+        if(SomeThingOn == true)
+        {
+            Spr.color = color_;
+        }
+        else
+        {
+            Spr.color = Color.white;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("yep");
+        SomeThingOn = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        SomeThingOn = false;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        SomeThingOn = true;
+    }
+}
