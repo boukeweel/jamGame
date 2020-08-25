8441028d1015f4a0b7162227b5aa90bca38f88b3
Bouke <BoukeWeel@outlook.com>
2020-08-25 19:58:17 +0200

    some edits

---
+++ jam game/Assets/scripts/Doos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doos : MonoBehaviour
+{
+
+    public bool Holding;
+    private Collider2D col_;
+    private void Update()
+    {
+        if(Holding == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this.transform.parent = col_.transform;
+
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+                this.transform.parent = null;
+        }
+        else
+        {
+            this.transform.parent = null;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("au");
+        
+        if (col.gameObject.tag == "Player")
+        {
+            col_ = col.collider;
+            Holding = true;
+        }
+        
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+            Holding = false;
+    }
+}
